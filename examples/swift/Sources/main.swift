d4824cfc5aa1cc064d9eb11ade439812049c4c65
Henry Allsuch <henry@boonandmoil.dev>
2025-05-14 20:59:10 +0100

    The owl's eyes animate

--- examples/swift/Sources/main.swift
+++ examples/swift/Sources/main.swift
@@ -17,8 +17,15 @@ func animateOwl() {
     // Terminal width to determine animation range
     let terminalWidth = 64
     
+    // Different eye states for animation
+    let eyeStates = ["@", "-", "o", "*"]
+    
     for position in 0...terminalWidth {
-        printOwl(offset: position)
+        // Cycle through eye states based on position
+        let eyeIndex = position % eyeStates.count
+        let currentEye = eyeStates[eyeIndex]
+        
+        printOwl(offset: position, eye: currentEye)
         // Sleep to control animation speed
         usleep(100000) // 100ms delay
     }
