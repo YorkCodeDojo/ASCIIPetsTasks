be6e36ebcab2acf70cf6f2fef5a2d3018ca36d9d
Henry Allsuch <henry@boonandmoil.dev>
2025-05-14 20:56:42 +0100

    The begins to move!

--- examples/swift/Sources/main.swift
+++ examples/swift/Sources/main.swift
@@ -1,11 +1,27 @@
+import Foundation
 
-func printOwl(){
+func printOwl(offset: Int, eye: String = "@") {
+
+    print("\u{001B}[2J\u{001B}[H", terminator: "")
+    
+    let padding = String(repeating: " ", count: offset)
     print("""
- ___ 
-<@,@>
-[`-']
--"-"-
-""")
+    \(padding) ___ 
+    \(padding)<\(eye),\(eye)>
+    \(padding)[`-']
+    \(padding)-"-"-
+    """)
 }
 
-printOwl()
+func animateOwl() {
+    // Terminal width to determine animation range
+    let terminalWidth = 64
+    
+    for position in 0...terminalWidth {
+        printOwl(offset: position)
+        // Sleep to control animation speed
+        usleep(100000) // 100ms delay
+    }
+}
+
+animateOwl()
